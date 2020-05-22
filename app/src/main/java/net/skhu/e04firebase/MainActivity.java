ec2a0224392d9403a0cc3ff9b7d3c331ead1aef9
Park HyeonJin <hhjj9798@gmail.com>
2020-05-22 21:16:43 +0900

    E04Firebase 첫 예제

---
+++ app/src/main/java/net/skhu/e04firebase/MainActivity.java
@@ -0,0 +1,33 @@
+package net.skhu.e04firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button_clicked(View view) {
+        Class classObj = null;
+        switch(view.getId()) {
+            case R.id.button1:
+                classObj =  Firebase1Activity.class;
+                break;
+            case R.id.button2:
+                classObj = MemoList1Activity.class;
+                break;
+            case R.id.button3:
+                classObj = MemoList2Activity.class;
+                break;
+        }
+        Intent intent = new Intent(this, classObj);
+        startActivity(intent);
+    }
+}
